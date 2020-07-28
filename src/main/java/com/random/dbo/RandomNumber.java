8afbf16aa186b615b80b0dd8420100ad48fc5e04
Almat <shampik807@gmail.com>
2020-07-29 00:07:46 +0600

    add some fix

--- src/main/java/com/random/dbo/RandomNumber.java
+++ src/main/java/com/random/dbo/RandomNumber.java
@@ -1,17 +1,28 @@
 package com.random.dbo;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 @Table(name = "random")
-public class RandomNumber {
+public class RandomNumber implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nickname")
+    @NotEmpty
     private String nickname;
+
+    @Column(name = "date")
+    @NotEmpty
     private Date createDate;
+
+    @Column(name = "value")
+    @NotEmpty
     private int value;
 
     public Long getId() {
